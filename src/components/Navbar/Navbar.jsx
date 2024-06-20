a371bf36a423af75fcd524e59110f7c2e774ecf2
luisced <luis.cedillo.maldonado@gmail.com>
2024-06-19 22:03:19 -0600

    feat: Added styles and responsivness to navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,34 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
-import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { FaGithub, FaLinkedin, FaBars, FaTimes } from "react-icons/fa";
 
-const Navbar = () => (
-	<nav className="navbar">
-		<ul className="navbar-links">
-			<li>
-				<a href="#home">.home()</a>
-			</li>
-			<li>
-				<a href="#about">.about()</a>
-			</li>
-			<li>
-				<a href="#portfolio">.portfolio()</a>
-			</li>
-			<li>
-				<a href="#contact">.contact()</a>
-			</li>
-			<li className="navbar-contact">
-				<a href="https://github.com/luisced">
-					<FaGithub className="icon" />
-				</a>
-			</li>
-			<li className="navbar-contact">
-				<a href="https://www.linkedin.com/in/luisced/">
-					<FaLinkedin className="icon" />
-				</a>
-			</li>
-		</ul>
-	</nav>
-);
+const Navbar = () => {
+	const [isOpen, setIsOpen] = useState(false);
+
+	const toggleNavbar = () => {
+		setIsOpen(!isOpen);
+	};
+
+	return (
+		<nav className="navbar">
+			<div className="hamburger" onClick={toggleNavbar}>
+				{isOpen ? <FaTimes /> : <FaBars />}
+			</div>
+			<ul className={`navbar-links ${isOpen ? "active" : ""}`}>
+				<li>
+					<a href="#home">.home()</a>
+				</li>
+				<li>
+					<a href="#about">.about()</a>
+				</li>
+				<li>
+					<a href="#portfolio">.portfolio()</a>
+				</li>
+				<li>
+					<a href="#contact">.contact()</a>
+				</li>
+				<li className="navbar-contact">
+					<a href="https://github.com/luisced">
+						<FaGithub className="icon" />
+					</a>
+				</li>
+				<li>
+					<a href="https://linkedin.com/in/luisced">
+						<FaLinkedin className="icon" />
+					</a>
+				</li>
+			</ul>
+		</nav>
+	);
+};
 
 export default Navbar;
