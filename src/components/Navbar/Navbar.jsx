a89a8d869f308444ee1d27e629f621ad097bc795
luisced <luis.cedillo.maldonado@gmail.com>
2024-06-20 21:18:17 -0600

    feat: added acrylic whole screen nav bar in responsive layout

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -10,25 +10,28 @@ const Navbar = () => {
 	};
 
 	return (
-		<nav className="navbar">
-			<div className="hamburger" onClick={toggleNavbar}>
-				{isOpen ? <FaTimes /> : <FaBars />}
-			</div>
-			<ul className={`navbar-links ${isOpen ? "active" : ""}`}>
-				<li>
-					<a href="#home">.home()</a>
-				</li>
-				<li>
-					<a href="#about">.about()</a>
-				</li>
-				<li>
-					<a href="#portfolio">.portfolio()</a>
-				</li>
-				<li>
-					<a href="#contact">.contact()</a>
-				</li>
-			</ul>
-		</nav>
+		<>
+			{isOpen && <div className="overlay" onClick={toggleNavbar}></div>}
+			<nav className="navbar">
+				<div className="hamburger" onClick={toggleNavbar}>
+					{isOpen ? <FaTimes /> : <FaBars />}
+				</div>
+				<ul className={`navbar-links ${isOpen ? "active" : ""}`}>
+					<li>
+						<a href="#home">.home()</a>
+					</li>
+					<li>
+						<a href="#about">.about()</a>
+					</li>
+					<li>
+						<a href="#portfolio">.portfolio()</a>
+					</li>
+					<li>
+						<a href="#contact">.contact()</a>
+					</li>
+				</ul>
+			</nav>
+		</>
 	);
 };
 
