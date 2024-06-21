f4e0a0e14d8a611ba0fba85148ef808c32186dfd
luisced <luis.cedillo.maldonado@gmail.com>
2024-06-21 15:43:56 -0600

    feat: first test of parallax scrolling

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
-import React from "react";
-import ParallaxWrapper from "./ParallaxWrapper";
+// App.js
+
+import React, { useEffect, useState } from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
-// import Ideas from "./components/Ideas";
-// import CV from "./components/CV";
-// import Backend from "./components/Backend";
-// import Web from "./components/Web";
-// import Mobile from "./components/Mobile";
-// import Contact from "./components/Contact";
+import Spline from "@splinetool/react-spline";
+import "./App.css";
 
 function App() {
+	const [background, setBackground] = useState("--color-background");
+
+	const handleScroll = () => {
+		const homeHeight = document.getElementById("home").clientHeight;
+		const scrollPosition = window.scrollY;
+	};
+
+	useEffect(() => {
+		window.addEventListener("scroll", handleScroll);
+		return () => {
+			window.removeEventListener("scroll", handleScroll);
+		};
+	}, []);
+
+	useEffect(() => {
+		document.body.style.backgroundColor = `var(${background})`;
+	}, [background]);
+
 	return (
 		<div className="App">
 			<Navbar />
 			<main>
-				<Home />
-
+				<div id="home">
+					<Home />
+				</div>
+				<div className="transition"></div>
+				<Spline scene="https://prod.spline.design/gh5W8uTjkvs-oVuN/scene.splinecode" />
 				<About />
 			</main>
 		</div>
