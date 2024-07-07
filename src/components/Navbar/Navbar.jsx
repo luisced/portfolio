ca54c84b68a89ef77906b2e3138a24e31ab382e2
luisced <luis.cedillo.maldonado@gmail.com>
2024-07-07 15:21:59 -0600

    fix: added responsive for both portrati and landscape layout navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,39 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import { FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
 	const [isOpen, setIsOpen] = useState(false);
-	const [activeSection, setActiveSection] = useState("");
 
 	const toggleNavbar = () => {
 		setIsOpen(!isOpen);
 	};
-
-	useEffect(() => {
-		const sections = document.querySelectorAll("section");
-		const options = {
-			threshold: 0.7,
-		};
-
-		const observer = new IntersectionObserver((entries) => {
-			entries.forEach((entry) => {
-				if (entry.isIntersecting) {
-					setActiveSection(entry.target.id);
-				}
-			});
-		}, options);
-
-		sections.forEach((section) => {
-			observer.observe(section);
-		});
-
-		return () => {
-			sections.forEach((section) => {
-				observer.unobserve(section);
-			});
-		};
-	}, []);
 
 	return (
 		<>
@@ -42,38 +16,16 @@ const Navbar = () => {
 				<div className="hamburger" onClick={toggleNavbar}>
 					{isOpen ? <FaTimes /> : <FaBars />}
 				</div>
-				<ul className={`navbar-links ${isOpen ? "active" : ""}`}>
+				<ul className="navbar-links">
 					<li>
-						<a
-							href="/#home"
-							className={activeSection === "home" ? "active-link" : ""}
-						>
-							.home()
-						</a>
+						<a href="/#home">.home()</a>
+					</li>
+
+					<li>
+						<a href="/portfolio">.portfolio()</a>
 					</li>
 					<li>
-						<a
-							href="/#about"
-							className={activeSection === "about" ? "active-link" : ""}
-						>
-							.about()
-						</a>
-					</li>
-					<li>
-						<a
-							href="/portfolio"
-							className={activeSection === "portfolio" ? "active-link" : ""}
-						>
-							.portfolio()
-						</a>
-					</li>
-					<li>
-						<a
-							href="#contact"
-							className={activeSection === "contact" ? "active-link" : ""}
-						>
-							.contact()
-						</a>
+						<a href="#contact">.contact()</a>
 					</li>
 				</ul>
 			</nav>
